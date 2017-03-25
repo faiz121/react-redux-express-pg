c291d06b72808c5668680018613bae06fa592a14
Faiz Mohammed <faizmohammed178@gmail.com>
2017-03-25 13:34:44 -0700

    Added search feature

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,20 @@ import React from 'react'
 const Home = React.createClass({
   render () {
     let input;
-    const { addTodo } = this.props;
+    const {addTodo, searchTerm, handleSearchTermChange} = this.props;
     return (
         <div className="text-center">
-          <input ref={(node) => input=node } type="text"  name="todo" placeholder='add a todo ...' />
-          <button  className="btn btn-primary btn-sm" onClick={() => {
+          <input ref={(node) => input = node } type="text" name="todo" placeholder='add a todo ...'/>
+          <button className="btn btn-primary btn-sm" onClick={() => {
             addTodo(input.value);
             input.value = '';
-          }} type="submit">add</button>
+          }} type="submit">add
+          </button>
+          <div>
+            <input type="text"
+                   onChange={handleSearchTermChange}
+                    placeholder='search ...'/>
+          </div>
         </div>
     )
   }
--- src/client.js
+++ src/client.js
@@ -8,7 +8,8 @@ import axios from 'axios'
 const App = React.createClass({
   getInitialState () {
     return {
-      todos: []
+      todos: [],
+      searchTerm: ''
     }
   },
 
@@ -19,23 +20,27 @@ const App = React.createClass({
         })
         .catch((error) => console.error('axios error', error));
   },
+  handleSearchTermChange (event) {
+    console.log(`search term value --> ${event.target.value}`);
+    this.setState({
+      searchTerm: event.target.value
+    })
+  },
 
   addTodo (task) {
     axios.post('http://localhost:3000/api/todos/', {"task" : task})   //[{"_id":"58d346b2f70e8d47a740112e","task":"get milk","__v":0}]
         .then((res) => {
-          console.log(`addTodo after POST ${JSON.stringify(res)}`);
           this.state.todos.push(res.data);
           this.setState({todos: this.state.todos});
         });
   },
 
   removeTodo (id) {
-    console.log(`id in remoteTodo is ${id}`);
     const remainder = this.state.todos.filter((todo) => {
       if(todo._id !== id) return todo;
     });
 
-    axios.delete('http://localhost:3000/api/todos' + '/' +id)
+    axios.delete('http://localhost:3000/api/todos/' +id)
         .then((res) => {
           this.setState({todos: remainder});
         });
@@ -45,17 +50,21 @@ const App = React.createClass({
     return (
         <div className="container">
         <h1 className="text-center">To Do List</h1>
-        <Home addTodo={this.addTodo}/>
+        <Home addTodo={this.addTodo} handleSearchTermChange={this.handleSearchTermChange}
+              searchTerm={this.state.searchTerm}/>
           <table className="table table-xs">
             <thead className="thead-default">
             <tr>
-            <th>To-do</th>
-            <th>Delete</th>
+              <th>To-do</th>
+              <th>Delete</th>
             </tr>
             </thead>
             <tbody>
-          {this.state.todos.map((todo) => {
-            console.log(`In this.state.todos.map ${JSON.stringify(todo)}`);
+          {this.state.todos
+              .filter((todo)=> {
+                return `${todo.task}`.indexOf(this.state.searchTerm) > -1;
+              })
+              .map((todo) => {
             return (
              <TodoList key={todo._id} todo={todo.task} todoId={todo._id} remove={this.removeTodo}/>
             )
