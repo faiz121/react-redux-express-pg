5076c1f73f43e087b93d01057c341c1a48ad5fc0
Ryan Perry <Rperry2174@gmail.com>
2018-08-05 23:47:02 -0700

    Add Cors

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from __future__ import division, print_function, absolute_import
 from flask import Flask, render_template, request, json, jsonify
+from flask_cors import CORS
 import sys
 
 app = Flask(__name__)
+CORS(app)
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
@@ -11,9 +13,10 @@ def index():
     return "Use /api/action"
 
 # http://localhost:4002/api/process_image?dataUrl=DATA_URL
-@app.route("/api/process_image")
+@app.route("/process_image")
 def process_image():
     dataUrl = request.args.get('dataUrl', '')
+    eprint(dataUrl)
     return jsonify({ 'dataUrl': dataUrl })
 
 if __name__ == "__main__":
